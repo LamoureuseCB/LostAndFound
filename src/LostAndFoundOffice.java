b102f1fdf7296764c229d42be1b949193764070d
LamoureuseCB <148466239+LamoureuseCB@users.noreply.github.com>
2024-04-21 09:57:36 +0600

    Initial commit

---
+++ src/LostAndFoundOffice.java
@@ -0,0 +1,29 @@
+
+public class LostAndFoundOffice {
+    Object[] things = new Object[6];
+    int index = 0;
+
+
+    public void put(Object item) {
+        if (item != null) {
+            things[index] = item;
+            index++;
+        } else {
+            System.out.println("Бюро(массив) заполнен");
+        }
+    }
+
+    public boolean check(Object target) {
+        if (target == null) {
+            System.out.println("Не указан предмет");
+            return false;
+        }
+        for (Object object : things) {
+            if (object != null && object.equals(target)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
